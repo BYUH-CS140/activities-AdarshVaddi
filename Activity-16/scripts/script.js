f1a92036ec8d26f983e62fffc27d5027e2869b9b
AdarshVaddi <adarshvaddi@go.byuh.edu>
2025-06-16 19:01:16 -1000

    1

---
+++ Activity-16/scripts/script.js
@@ -0,0 +1,5 @@
+function updateColor(color) {
+    document.getElementById('displayArea').style.backgroundColor = color;
+    document.body.style.backgroundColor = color;
+    document.getElementById('colorValue').textContent = color;
+}
